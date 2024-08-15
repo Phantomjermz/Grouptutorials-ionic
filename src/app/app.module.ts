318a9cf6781e95e2c7e0cc2729e1d5d18fd1eb11
Phantomjermz <jermaine-brown@hotmail.com>
2024-08-15 17:12:13 -0400

    update 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { FormsModule } from '@angular/forms';  // Import FormsModule
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomePage } from './home/home.page';  // Import HomePage if not already
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [AppComponent, HomePage],  // Declare HomePage here
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule],  // Add FormsModule to imports
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
